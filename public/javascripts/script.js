e6f47aebccab4c14edf3e40149b95712c1cec70d
unknown <iolegrai@gmail.com>
2015-01-18 15:27:20 +0200

    Description: added disabled functionality into btn. Version: 0.0.2. Browser: Chrome. JSLint: yes. Owner: Olha Todorashko. Remark: this app without data file, should add it to the data folder

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,28 +1,6 @@
 /**
  * Created by olegra on 14.01.2015.
  */
-var validateDate = (function () {
-    var startRangeDate = new Date(2006, 11, 16),
-        endRangeDate = new Date(2010, 11, 26);
-
-    return {
-        mdy: function (date) {
-            var date = date.split('/');
-            return new Date(date[2], date[0] - 1, date[1]);
-        },
-        validateRange: function (startDate, endDate) {
-            var startDate = this.mdy(startDate),
-                endDate = this.mdy(endDate);
-
-            if (startDate > endDate) {
-                return false;
-            }
-
-            return (startDate >= startRangeDate && endDate <= endRangeDate);
-        }
-    };
-}());
-
 $(document).ready(function () {
 
     var start = $("#date-picker-1").datepicker(),
@@ -31,7 +9,28 @@ $(document).ready(function () {
         errorDateBlock = $("#invalidDate"),
         load = $('#load'),
         calculBtn = $("#calcul"),
-        currentResults = null;
+        currentResults = null,
+        validateDate = (function () {
+            var startRangeDate = new Date(2006, 11, 16),
+                endRangeDate = new Date(2010, 11, 26);
+
+            return {
+                mdy: function (date) {
+                    var date = date.split('/');
+                    return new Date(date[2], date[0] - 1, date[1]);
+                },
+                validateRange: function (startDate, endDate) {
+                    var startDate = this.mdy(startDate),
+                        endDate = this.mdy(endDate);
+
+                    if (startDate > endDate) {
+                        return false;
+                    }
+
+                    return (startDate >= startRangeDate && endDate <= endRangeDate);
+                }
+            };
+        }());
 
     calculBtn.on("click", function () {
 
@@ -40,7 +39,7 @@ $(document).ready(function () {
 
         if (!validateDate.validateRange(startDate, endDate)) {
             container.empty();
-            errorDateBlock.toggle('slow').fadeOut(4000, function () {
+            errorDateBlock.toggle('slow').fadeOut(5000, function () {
                 start.val("");
                 end.val("");
             });
@@ -56,7 +55,7 @@ $(document).ready(function () {
             };
 
         container.empty();
-        calculBtn.prop('disabled', true);
+        calculBtn.attr('disabled', 'disabled');
         load.show();
 
         $.ajax({
@@ -69,7 +68,7 @@ $(document).ready(function () {
             },
             success: function (data) {
                 load.hide();
-                calculBtn.prop('disabled', true);
+                calculBtn.removeAttr('disabled');
                 currentResults = data;
                 /*console.log(data.length);*/
 
